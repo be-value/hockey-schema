5fcc9f9932c2c627f029eabd95f9b3f48f0efcc8
Jelle Be-Value <be-value@live.nl>
2019-08-25 20:30:20 +0200

    Added serialization/deserialization

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,35 @@ import "./App.css";
 import Schedule from "./components/Schedule/Schedule";
 import { ScheduleItem } from "./core/ScheduleItem";
 
-class App extends React.Component {
+function dateReviver(key: any, value: any): Date|string {
+  const dateFormat: any = /^\d{2}-\d{2}-\d{4}$/;
+  if (typeof value === "string" && dateFormat.test(value)) {
+    var components: string[] = value.split("-");
+    let day: number = Number(components[0]);
+    let monthIdx: number = Number(components[1])-1;
+    let year: number = Number(components[2]);
+    return new Date(year, monthIdx, day);
+  }
 
+  return value;
+}
+
+function dateReplacer(name: any, value: any): any {
+  if (name === "when") {
+    let date: Date = new Date(value);
+    return `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`;
+  }
+
+  return value;
+}
+
+
+class App extends React.Component {
   private getSchedule(): Array<ScheduleItem> {
-    let items: Array<ScheduleItem> = new Array<ScheduleItem>();
-    let item1: ScheduleItem = new ScheduleItem();
-    item1.when = new Date(2019, 9, 1);
-    item1.what = "Wedstrijd";
-    item1.where = "Waddinxveen";
-    item1.who = ["Aap", "Noot", "Mies"];
-    let item2: ScheduleItem = new ScheduleItem();
-    item2.when = new Date(2019, 9, 23);
-    item2.what = "Bardienst";
-    item2.where = "Rotterdam";
-    item2.who = ["Wim", "Zus", "Jet"];
-    items.push(item2);
-    items.push(item1);
-    console.log(JSON.stringify(items, null, 2));
+    // tslint:disable-next-line:quotemark
+    var serialized: string = '[{"when":"23-10-2019","what":"Bardienst","where":"Rotterdam","who":["Wim","Zus","Jet"]},{"when":"01-10-2019","what":"Wedstrijd","where":"Waddinxveen","who":["Aap","Noot","Mies"]}]';
+    let items: Array<ScheduleItem> = JSON.parse(serialized, dateReviver);
+    console.log(JSON.stringify(items, dateReplacer, 2));
     return items;
   }
 
