cdd92b09d70ce9b81f112f0845e2fa12665498c7
Jelle Be-Value <be-value@live.nl>
2019-09-07 12:42:21 +0200

    removed double referenced css

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { IAppProps } from "./IAppProps";
-import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
 import SchedulePage from "../SchedulePage/SchedulePage";
 
 const App: React.SFC<IAppProps> = (props) => {
